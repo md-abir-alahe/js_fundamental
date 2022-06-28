b9ea48829e7fe36d452dca30fb15a34227f76dc0
md-abir-alahe <mdabiralahe@gmail.com>
2022-06-28 15:36:25 +0600

    functionRetunAndInvokes

--- js_fundamental/JS Function/Learn_Javascript_function.js
+++ js_fundamental/JS Function/Learn_Javascript_function.js
@@ -1,5 +1,23 @@
 
-function myFunction(p1, p2){
+function myFunction1(p1, p2){
     return p1 * p2;
 }
-document.getElementById("demo1").innerHTML = myFunction(4, 3);
+document.getElementById("demo1").innerHTML = myFunction1(4, 3);
+
+
+var x = myFunction2(5,4);
+document.getElementById("demo2").innerHTML = x;
+function myFunction2(a,b){
+    return a * b;
+}
+
+
+
+function toCelsius(f) {
+    return (5/9)*(f-32);
+}
+document.getElementById("demo3").innerHTML = toCelsius(77);
+
+
+
+document.getElementById("demo4").innerHTML = toCelsius;
